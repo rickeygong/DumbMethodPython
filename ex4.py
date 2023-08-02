b446c5346bd1d5c58ea12d60d901287e0b3cefd2
gongdonghai <donghai.gong@outlook.com>
2023-08-02 22:28:23 +0800

    提交

---
+++ ex4.py
@@ -0,0 +1,12 @@
+# 变量和名字
+cars = 100
+space_in_a_car = 4.0
+drivers = 30 #司机
+passengers = 90 #乘客
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+
+
+print("There are" , cars , "cars available.")
+print("There are only" , drivers , "drivers available.")
---
+++ ex5.py
@@ -0,0 +1,11 @@
+# 更多变量和打印
+my_name = "Rickey"
+my_age = 25 # not a lie
+my_height = 163 #cm
+my_weight = 75 #kg
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "Black"
+
+print(f"大家好，我叫{my_name}")
+print(f"{my_name}的年龄是{my_age}")
